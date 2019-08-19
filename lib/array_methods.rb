779bd896f50175eb569ab9b6b3e7a91081c6c4a6
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-19 18:01:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,20 +1,13 @@
+
+
 def find_element_index(array, value_to_find) 
-  count= 0 
-  while count < array.length do 
-    return array.index(value_to_find)
-    count += 1 
+array.length.times do |count| 
+    if array[count] == value_to_find
+    return count 
   end 
+end 
   nil 
 end 
-
-# def find_element_index(array, value_to_find) 
-# array.length.times do |count| 
-#     if array[count] == value_to_find
-#     return count 
-#   end 
-# end 
-#   nil 
-# end 
 
 
 def find_max_value(array)
